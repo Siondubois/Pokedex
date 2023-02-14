2dbf67d2fc84405a486d4f32c964ec9b1ef9039b
Siondubois <sion.p.ly@gmail.com>
2023-02-14 12:56:39 +0100

    auth isloggedin

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../typings';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  static IS_LOGGED_IN = 'isLoggedIn';
 
   private apiUrl = "https://pokedex-c8855-default-rtdb.europe-west1.firebasedatabase.app";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private storageService: StorageService) { }
 
   signIn(user: User) {
     return this.http.post(`${this.apiUrl}/users.json`, user);
@@ -19,4 +21,7 @@ export class AuthService {
     return this.http.get(`${this.apiUrl}/users.json`);
   }
 
+  storeIsLoggedIn(value: string) {
+    this.storageService.setItem(AuthService.IS_LOGGED_IN, value);
+  }
 }
