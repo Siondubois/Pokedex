b2a61a87b0871171f40cd9bdd3989a9ce7c5a078
thomasbetend <thomas.betend@yahoo.fr>
2023-02-15 17:08:20 +0100

    unAuth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+import { UnAuthGuard } from './guards/un-auth.guard';
 import { ConnexionComponent } from './pages/connexion/connexion.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PokedexComponent } from './pages/pokedex/pokedex.component';
@@ -13,15 +14,18 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
+    canActivate: [AuthGuard],
     path: 'pokedex/:page',
     component: PokedexComponent,
   },
   {
+    canActivate: [UnAuthGuard],
     path:'login',
     component: ConnexionComponent,
   },
   {
-    path:'signin',
+    canActivate: [UnAuthGuard],
+    path:'signup',
     component: SigninComponent,
   }
 ];
